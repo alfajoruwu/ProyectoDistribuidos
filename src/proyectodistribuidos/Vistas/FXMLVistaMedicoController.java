5bd9ebcb6711bb9a1264d15d7faacaeb81277040
Antonia <afuenzalida20@alumnos.utalca.cl>
2023-10-30 22:07:53 -0300

    Botón de salida

--- src/proyectodistribuidos/Vistas/FXMLVistaMedicoController.java
+++ src/proyectodistribuidos/Vistas/FXMLVistaMedicoController.java
@@ -2,16 +2,28 @@ package proyectodistribuidos.Vistas;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldListCell;
+import javafx.stage.Stage;
 import javafx.util.StringConverter;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FXMLVistaMedicoController implements Initializable {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
 
     @FXML
     private ListView<String> listaContactos;
@@ -45,6 +57,21 @@ public class FXMLVistaMedicoController implements Initializable {
 
     @FXML
     private Label tituloEncabezadoMedico;
+
+    @FXML
+    private Label chatGeneral;
+
+    @FXML
+    private Button botonSalidar;
+
+    @FXML
+     public void irAVistaLogin(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/proyectodistribuidos/Login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     private ObservableList<String> contactList = FXCollections.observableArrayList(
         "Pabellón",
