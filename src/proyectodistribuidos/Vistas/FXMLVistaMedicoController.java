58dc4c02b60141b1f8cc115973cc58806899c432
Antonia <afuenzalida20@alumnos.utalca.cl>
2023-10-30 21:24:01 -0300

    Interfaz mensajes
    Interfaz solo de los mensajes

--- src/proyectodistribuidos/Vistas/FXMLVistaMedicoController.java
+++ src/proyectodistribuidos/Vistas/FXMLVistaMedicoController.java
@@ -1,24 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectodistribuidos.Vistas;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author alfajor
- */
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+
 public class FXMLVistaMedicoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button botonPabellon;
+
+    @FXML
+    private Button botonAdmision;
+
+    @FXML
+    private Button botonExamenes;
+
+    @FXML
+    private Button botonAuxiliar;
+
+    @FXML
+    private Button botonMedicos;
+
+    @FXML
+    private TextArea chatArea;
+    
+    @FXML
+    private TextField messageField;
+    
+    @FXML
+    private Button sendButton;
+    
+    public void initialize() {
+        // Inicializa tu controlador aquí
+    }
+    
+    @FXML
+    private void handleSendButtonAction() {
+        String message = messageField.getText();
+        if (!message.isEmpty()) {
+            chatArea.appendText("Tú: " + message + "\n");
+            messageField.clear();
+            // Aquí puedes agregar lógica para enviar el mensaje a otros usuarios
+        }
+    }
+
+    
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
