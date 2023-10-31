ed744e7070f34b2d0e48b53e9b3ca8e4b8075bad
Antonia <afuenzalida20@alumnos.utalca.cl>
2023-10-30 21:51:11 -0300

    Buscador de contacto
    interfaz del buscador de contacto

--- src/proyectodistribuidos/Vistas/FXMLVistaMedicoController.java
+++ src/proyectodistribuidos/Vistas/FXMLVistaMedicoController.java
@@ -1,17 +1,20 @@
-
 package proyectodistribuidos.Vistas;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.util.StringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-
-
 public class FXMLVistaMedicoController implements Initializable {
+
+    @FXML
+    private ListView<String> listaContactos;
 
     @FXML
     private Button botonPabellon;
@@ -30,33 +33,62 @@ public class FXMLVistaMedicoController implements Initializable {
 
     @FXML
     private TextArea chatArea;
-    
+
     @FXML
     private TextField messageField;
-    
+
     @FXML
-    private Button sendButton;
-    
-    public void initialize() {
-        // Inicializa tu controlador aquí
+    private Button botonEnviarMensaje;
+
+    @FXML
+    private TextField textoBuscarContacto;
+
+    @FXML
+    private Label tituloEncabezadoMedico;
+
+    private ObservableList<String> contactList = FXCollections.observableArrayList(
+        "Pabellón",
+        "Exámenes",
+        "Médico 1",
+        "Médico 2",
+        "Médico 3"
+    );
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        listaContactos.setItems(contactList);
+
+        listaContactos.setCellFactory(TextFieldListCell.forListView(new StringConverter<String>() {
+            @Override
+            public String toString(String contact) {
+                return contact;
+            }
+
+            @Override
+            public String fromString(String string) {
+                return string;
+            }
+        }));
+
+        textoBuscarContacto.textProperty().addListener((observable, oldValue, newValue) -> {
+            String searchTerm = newValue.toLowerCase();
+            ObservableList<String> filteredList = FXCollections.observableArrayList();
+            for (String contact : contactList) {
+                if (contact.toLowerCase().contains(searchTerm)) {
+                    filteredList.add(contact);
+                }
+            }
+            listaContactos.setItems(filteredList);
+        });
     }
-    
+
     @FXML
     private void handleSendButtonAction() {
         String message = messageField.getText();
         if (!message.isEmpty()) {
             chatArea.appendText("Tú: " + message + "\n");
             messageField.clear();
-            // Aquí puedes agregar lógica para enviar el mensaje a otros usuarios
+
         }
     }
-
-    
-
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
 }
