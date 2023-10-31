ca2217aec6c3059e0fc8539c66561a16fef8d0af
luckas <luckas.strnadalvarez@gmail.com>
2023-10-31 00:39:30 -0300

    borre cosas sobrantes

--- src/proyectodistribuidos/BaseDatos/Connect.java
+++ src/proyectodistribuidos/BaseDatos/Connect.java
@@ -39,15 +39,4 @@ public class Connect {
             System.out.println(ex.getMessage());
         }
     }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Connection connection = connect();
-        // Perform database operations here using the 'connection' object
-
-        // Close the connection when done
-        disconnect();
-    }
 }
