113d8bb8f3b82ea24419dbed2c43edd44c2e0446
luckas <luckas.strnadalvarez@gmail.com>
2023-12-18 22:13:36 -0300

    test 3

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,4 @@
 module proyecto2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
     requires transitive java.sql;
     requires transitive java.desktop;
 
